734ee019b4f0c44c85f4f2da772a7ad351de0b2e
Veron <veronkyn@msn.com>
2019-02-25 16:04:32 +0800

    Changed Contact App - contact_date field

--- contacts/models.py
+++ contacts/models.py
@@ -10,7 +10,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
-    contact_date = models.DateTimeField(default=timezone.now, blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
     # contact_date = models.DateTimeField(default=now, blank=True)
     user_id = models.IntegerField(blank=True)
 
